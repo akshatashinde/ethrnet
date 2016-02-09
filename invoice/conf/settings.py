d4c15851c70772e95c952707f8eb812c49458b42
Ubuntu <ubuntu@ip-172-31-21-43.us-west-2.compute.internal>
2016-02-09 18:51:23 +0000

    changes done

--- invoice/conf/settings.py
+++ invoice/conf/settings.py
@@ -4,8 +4,8 @@ from django.conf import settings
 
 
 INV_MODULE = getattr(settings, 'INV_MODULE', 'invoice.pdf')
-INV_LOGO = getattr(settings, 'INV_LOGO', path.join('/home/vishal/ethernet/ethrnet/static/img/ethernet.png'))
-FOOTER_LOGO = getattr(settings, 'FOOTER_LOGO', path.join('/home/vishal/ethernet/ethrnet/static/img/ethernet_footer.png'))
+INV_LOGO = getattr(settings, 'INV_LOGO', path.join('/var/www/ethernet/ethrnet/static/img/ethernet.png'))
+FOOTER_LOGO = getattr(settings, 'FOOTER_LOGO', path.join('/var/www/ethernet/ethrnet/static/img/ethernet_footer.png'))
 INV_CURRENCY = getattr(settings, 'INV_CURRENCY', u'EUR')
 INV_CURRENCY_SYMBOL = getattr(settings, 'INV_CURRENCY_SYMBOL', u'€')
 INV_EMAIL_SUBJECT = getattr(settings, 'INV_EMAIL_SUBJECT', u'[%s] Invoice %%(invoice_id)s' % (settings.SITE_NAME))
